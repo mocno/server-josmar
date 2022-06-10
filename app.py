e19f1ed8e817e4252cc1cb519243688e408a7233
mocno <65563015+mocno@users.noreply.github.com>
2022-06-10 15:11:41 -0300

    Create a /user/get route

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
 from Flask import Flask
+import db
 
 def create_app():
     return Flask(__name__)
-
-class JosmarUser():
-    def __init__(self, _id: int, name: str, access: int):
-        self.id = _id
-        self.name = name
-        self.access = access
-        self.current_keys = []
-
-class JosmarKey():
-    def __init__(self, _id: int, name: str, room: JosmarRoom):
-        self.id = _id
-        self.name = name
-        self.room = room
-
-class JosmarRoom():
-    def __init__(self, _id: int, name: str):
-        self.id = _id
-        self.name = name
-        self._keys = []
 
 def main():
     app = create_app()
@@ -29,24 +11,55 @@ def main():
     # Aplicativo: josmar-app
     # - pega informações do usuario (ele mesmo)
     # - situação atual das salas
-    # - pega informação das chaves
+    # - pega informação da chave
     #
     # Armario: e-josmar
     # - recebe atualização dos dados da posição das chaves (na devolução e no uso das chaves)
     # - faz as verificações de usuario (~~)
     #
 
-    app.route('/user/', methods=['GET'])
+    @app.route('/user/get', methods=['GET'])
     def get_user():
-        return [
-            'user'
-        ]
+        user_id = request.args.get('id', None)
 
-    app.route('/user/', methods=['POST'])
-    def edit_user():
-        return [
-            'user'
-        ]
+        if user_id is None:
+            return {
+                'error': 'Id esta vazio, adicione um para pesquisa',
+                'status': 'error'
+            }
+
+        try:
+            with db.create_session() as session:
+                user = session.filter(db.User.id == user_id).first()
+
+                if user is None:
+                    return {
+                        'error': 'Usuário não foi encontrado, foi morto',
+                        'status': 'error'
+                    }
+
+                return {
+                    'user': dict(user),
+                    'status': 'ok'
+                }
+        except (SQLAlchemyError, DisconnectionError):
+            return {
+                'error': 'Error interno, erro de conexão do banco de dados',
+                'status': 'error'
+            }
+
+    @app.route('/room/map', methods=['GET'])
+    def rooms_map():
+        pass
+
+    @app.route('/key/get', methods=['GET'])
+    def key_get():
+        pass
+
+    @app.route('/key/update', methods=['POST'])
+    def key_update():
+        pass
+        
 
 if __name__ == '__main__':
     main()
