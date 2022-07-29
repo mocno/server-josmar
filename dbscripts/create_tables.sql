ffee89faad57350b068280a28610ae290071842f
Mocno <gustavo.correa.mogi@gmail.com>
2022-07-29 16:06:02 -0300

    Fix sql syntax of create_tables

--- dbscripts/create_tables.sql
+++ dbscripts/create_tables.sql
@@ -2,24 +2,24 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS keys;
 DROP TABLE IF EXISTS rooms;
 
-CREATE TABLE users {
+CREATE TABLE users (
     id serial PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(50) NOT NULL,
     access INTEGER,
 	created_on TIMESTAMP NOT NULL,
     last_login TIMESTAMP
-}
+);
 
-CREATE TABLE keys {
+CREATE TABLE keys (
     id serial PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
     -- room = Column(Integer, ForeignKey('room.id'))
     -- user = Column(Integer, ForeignKey('user.id'))
-}
+);
 
 
-CREATE TABLE rooms {
+CREATE TABLE rooms (
     id serial PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
-}
+    name VARCHAR(50) UNIQUE NOT NULL
+);
