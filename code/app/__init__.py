4ff16ed2175a0374fa29438d0b697b5fb8295709
Mocno <gustavo.correa.mogi@gmail.com>
2022-07-29 16:12:15 -0300

    Remove functions of app

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -5,66 +5,56 @@ from flask import Flask, request
 from sqlalchemy.exc import DisconnectionError
 import db
 
-def create_app():
-    """Create flask app"""
-    app = Flask(__name__)
+"""Create flask app"""
+app = Flask(__name__)
 
-    app.config["POSTGRES_URI"] = f"postgresql+psycopg2://{os.environ['DB_USERNAME']}:" \
-        f"{os.environ['DB_PASSWORD']}@{os.environ['DB_HOSTNAME']}:{os.environ['DB_PORT']}" \
-        f"/{os.environ['DB_DATABASE']}"
+app.config["POSTGRES_URI"] = f"postgresql+psycopg2://{os.environ['DB_USERNAME']}:" \
+    f"{os.environ['DB_PASSWORD']}@{os.environ['DB_HOSTNAME']}:{os.environ['DB_PORT']}" \
+    f"/{os.environ['DB_DATABASE']}"
 
-    return app
+@app.route("/")
+def index():
+    """Index page"""
+    return "Hello, World!"
 
-def start_app():
-    """Start flask app with api routes"""
+@app.route('/user/get', methods=['GET'])
+def get_user():
+    user_id = request.args.get('id', None)
 
-    app = create_app()
+    if user_id is None:
+        return {
+            'error': 'No user id provided',
+            'status': 'error'
+        }
 
-    @app.route('/user/get', methods=['GET'])
-    def get_user():
-        user_id = request.args.get('id', None)
+    try:
+        with db.create_session(app.config["POSTGRES_URI"]) as session:
+            user = session.filter(db.User.id == user_id).first()
 
-        if user_id is None:
-            return {
-                'error': 'Id esta vazio, adicione um para pesquisa',
-                'status': 'error'
-            }
-
-        try:
-            with db.create_session(app.config["POSTGRES_URI"]) as session:
-                user = session.filter(db.User.id == user_id).first()
-
-                if user is None:
-                    return {
-                        'error': 'Usuário não foi encontrado, foi morto',
-                        'status': 'error'
-                    }
-
+            if user is None:
                 return {
-                    'user': dict(user),
-                    'status': 'ok'
+                    'error': 'User not found',
+                    'status': 'error'
                 }
-        except DisconnectionError:
+
             return {
-                'error': 'Error interno, erro de conexão do banco de dados',
-                'status': 'error'
+                'user': dict(user),
+                'status': 'ok'
             }
+    except DisconnectionError:
+        return {
+            'error': 'Database connection error',
+            'status': 'error'
+        }
 
-    @app.route('/room/map', methods=['GET'])
-    def rooms_map():
-        pass
+@app.route('/room/map', methods=['GET'])
+def rooms_map():
+    pass
 
-    @app.route('/key/get', methods=['GET'])
-    def key_get():
-        pass
+@app.route('/key/get', methods=['GET'])
+def key_get():
+    pass
 
-    @app.route('/key/update', methods=['POST'])
-    def key_update():
-        pass
-
-    environment_debug = os.environ.get("APP_DEBUG", "False") == "True"
-    environment_port = os.environ.get("APP_PORT", 5000)
-    app.run(host='0.0.0.0', port=environment_port, debug=environment_debug)
-
-if __name__ == '__main__':
-    start_app()
+@app.route('/key/update', methods=['POST'])
+def key_update():
+    pass
